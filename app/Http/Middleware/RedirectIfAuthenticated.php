78e266cde260999f6ea96d47cb08cdb67052f01f
Tusartus <arthus8ni@gmail.com>
2020-05-26 19:47:28 +0200

    herokuadd

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,7 @@ class RedirectIfAuthenticated
     {
         if (Auth::guard($guard)->check() && Auth::user()->role->id == 1) {
             return redirect()->route('admin.dashboard');
-        } elseif(Auth::guard($guard)->check() && Auth::user()->role->id == 1){
+        } elseif(Auth::guard($guard)->check() && Auth::user()->role->id == 2){
               return redirect()->route('author.dashboard');
         }else{
           return $next($request);
